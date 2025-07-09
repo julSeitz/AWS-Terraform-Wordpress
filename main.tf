cd6ad9333925233354c2e9e26aa0f3230750dafc
Julian Seitz <112495164+julSeitz@users.noreply.github.com>
2025-07-09 11:32:42 +0200

    Added EC2 instance setup

--- main.tf
+++ main.tf
@@ -40,3 +40,41 @@ resource "aws_route" "publicRoute" {
     route_table_id = aws_vpc.myVpc.main_route_table_id
     destination_cidr_block = "0.0.0.0/0"
 }
+
+resource "aws_security_group" "allow_ssh" {
+    name = "allow_ssh"
+    description = "Allow inbound SSH traffic and all outbound traffic"
+    vpc_id = aws_vpc.myVpc.id
+
+    tags = {
+        Name = "allow_ssh"
+    }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_rule" {
+    security_group_id = aws_security_group.allow_ssh.id
+    from_port = 22
+    to_port = 22
+    ip_protocol = "tcp"
+    cidr_ipv4 = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_instance" "myInstance" {
+    ami = "ami-05ee755be0cd7555c"
+    instance_type = "t3.micro"
+    key_name = "vockey"
+    subnet_id = aws_subnet.publicSubnet.id
+    security_groups = [aws_security_group.allow_ssh.id]
+    associate_public_ip_address = "true"
+
+    tags =  {
+        Name = "MyInstanceViaTerraform"
+        OS = "Linux"
+    }
+}
