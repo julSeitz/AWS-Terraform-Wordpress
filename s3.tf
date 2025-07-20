e8122c2cb295d0851ab4a412df67fc0fa6fac33d
Julian Seitz <112495164+julSeitz@users.noreply.github.com>
2025-07-20 11:50:40 +0200

    Added static s3 website showing downtime message for savings mode

--- s3.tf
+++ s3.tf
@@ -1,3 +1,30 @@
 resource "aws_s3_bucket" "wordpress_application_data_bucket" {
   bucket = var.wordpress_application_bucket_name
 }
+
+resource "aws_s3_bucket" "scheduled_downtime_static_message_bucket" {
+
+}
+
+resource "aws_s3_bucket_website_configuration" "scheduled_downtime_static_message" {
+  bucket = aws_s3_bucket.scheduled_downtime_static_message_bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "scheduled_downtime_static_message_public_access" {
+  bucket                  = aws_s3_bucket.scheduled_downtime_static_message_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_object" "scheduled_downtime_static_message_object" {
+  bucket       = aws_s3_bucket.scheduled_downtime_static_message_bucket.id
+  key          = "index.html"
+  source       = "Static-Message-Website/index.html"
+  etag         = filemd5("Static-Message-Website/index.html")
+  content_type = "text/html"
+}
